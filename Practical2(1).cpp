74da73b621aafc15f8fd98fb89970a6ec0dbe2d8
Krupasuthar66 <66459383+Krupasuthar66@users.noreply.github.com>
2022-05-07 12:51:16 +0530

    Create Practical2(1).cpp

---
+++ Practical2(1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b; 	
+  cout << "Input a:"; 
+  cin >> a; 	
+  cout << "Input b:"; 
+  cin >> b; 	
+  cout << "sum is" << a + b << endl; 	
+  cout << "product is " << a*b << endl; 
+	return 0;
+}
+
+// OUTPUT //
+Input a:45
+Input b:5
+sum is50
+product is 225
