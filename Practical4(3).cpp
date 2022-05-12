d6d7fc38900f872dad9eb9acdc50a1ab265043ef
Krupa Suthar <66459383+Krupasuthar66@users.noreply.github.com>
2022-05-12 13:16:45 +0530

    Create Practical4(3).cpp

---
+++ Practical4(3).cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+using namespace std;  
+// create an integer Test() function contains an argument num  
+int Test (const int num)  
+{  
+// if we change the value of the const argument, it thwrows an error.  
+// num = num + 10;  
+cout << " The value of num: " << num << endl;  
+return 0;  
+}  
+int main ()  
+{  
+// call function  
+Test(5);  
+}  
+
+// OUTPUT //
+
+The value of num: 5
