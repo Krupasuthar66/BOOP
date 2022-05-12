68369e3e4ddfe6729717ab747807187b1d99dd44
Krupa Suthar <66459383+Krupasuthar66@users.noreply.github.com>
2022-05-12 14:00:08 +0530

    Create Practical5.cpp

---
+++ Practical5.cpp
@@ -0,0 +1,102 @@
+// Passing structure to function in C++
+
+#include <iostream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+
+void displayData(Person);   // Function declaration
+
+int main() {
+    Person p;
+
+    cout << "Enter Full name: ";
+    cin.get(p.name, 50);
+    cout << "Enter age: ";
+    cin >> p.age;
+    cout << "Enter salary: ";
+    cin >> p.salary;
+
+    // Function call with structure variable as an argument
+    displayData(p);
+
+    return 0;
+}
+
+void displayData(Person p) {
+    cout << "\nDisplaying Information." << endl;
+    cout << "Name: " << p.name << endl;
+    cout <<"Age: " << p.age << endl;
+    cout << "Salary: " << p.salary;
+}
+
+//  OUTPUT  //
+
+Enter Full name: Steven grant
+Enter age: 34
+Enter salary: 34234.5
+Displaying Information.
+Name: Steven grant
+Age: 34
+Salary: 34234.5
+
+
+//  Returning structure from function in C++
+
+#include <iostream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+
+Person getData(Person); 
+void displayData(Person); 
+
+int main() {
+
+    Person p, temp;
+
+    temp = getData(p);
+    p = temp;
+    displayData(p);
+
+    return 0;
+}
+
+Person getData(Person p) {
+
+    cout << "Enter Full name: ";
+    cin.get(p.name, 50);
+
+    cout << "Enter age: ";
+    cin >> p.age;
+
+    cout << "Enter salary: ";
+    cin >> p.salary;
+
+    return p;
+}
+
+void displayData(Person p) {
+    cout << "\nDisplaying Information." << endl;
+    cout << "Name: " << p.name << endl;
+    cout <<"Age: " << p.age << endl;
+    cout << "Salary: " << p.salary;
+}
+
+// OUTPUT //
+
+Enter Full name: Steven grant
+Enter age: 35
+Enter salary: 26537.6
+Displaying Information.
+Name: Steven grant
+Age: 35
+Salary: 26537.6
